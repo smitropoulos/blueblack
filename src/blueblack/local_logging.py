e0a681a3b649d4dcb97450480b4c1d120b6921b4
Stefanos Mitropoulos <stefmitropoulos@gmail.com>
2024-06-07 09:26:55 +0300

    fix: info logging default

--- src/blueblack/local_logging.py
+++ src/blueblack/local_logging.py
@@ -4,4 +4,4 @@ import logging
 
 logger = logging
 FORMAT = "[{asctime:<20} {filename:>20}:{lineno:<4} - {funcName:<20}]   {message}"
-logging.basicConfig(format=FORMAT, level=logging.DEBUG, style="{")
+logging.basicConfig(format=FORMAT, level=logging.INFO, style="{")
